39c8dc2e55a0d28be511eb956cb9e8a0e0817369
Martin <info@martinmohlmann.nl>
2020-06-17 19:14:17 +0200

    Added tcr scripts to repo

---
+++ tcr-script.sh
@@ -0,0 +1,54 @@
+RED="tput setaf 1"
+GREEN="tput setaf 2"
+YELLOW="tput setaf 3"
+MAGENTA="tput setaf 5"
+DEFAULT="tput sgr0"
+
+function Build() {
+    echo "$($MAGENTA)==== Build ====$($DEFAULT)"
+    dotnet build --nologo -v q
+}
+
+function Test() {
+    echo ""
+    echo "$($YELLOW)==== Test ====$($DEFAULT)"
+    echo ""
+    dotnet test --no-build --nologo
+}
+
+function Commit() {
+    echo ""
+    echo "$($GREEN)==== Commit ====$($DEFAULT)"
+    echo ""
+    git add .
+    git commit -m "WIP"
+    return 0
+}
+
+function Amend() {
+    echo "$($GREEN)==== Commit (amend) ====$($DEFAULT)"
+    echo ""
+    git commit -am "WIP" --amend --allow-empty
+}
+
+function Checkout() {
+    echo "$($RED)==== Revert (implementation) ====$($DEFAULT)"
+    echo ""
+    git checkout Kata/
+}
+
+function Revert() {
+    echo "$($RED)==== Revert ====$($DEFAULT)"
+    echo ""
+    git reset --hard
+}
+
+function AmendOrCommit() {
+    [[ $(git log -1 --pretty=format:%B) == WIP ]] && Amend || Commit
+}
+
+function TestCommitCheckout() {
+    (Test && AmendOrCommit) || Checkout
+}
+
+Build && Test && Commit || Revert
